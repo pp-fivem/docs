187c2654e05c86dce2bb6de57d14a3dad0a4dfc3
PEEVEE <67219253+PEEVEEz@users.noreply.github.com>
2024-01-04 00:14:44 +0200

    First

---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+  logo: (
+    <img
+      width={60}
+      src="https://cdn.discordapp.com/attachments/946360401124294696/1192226603275661352/img_341476_2023_10_17_19_37_37-removebg-preview.png?ex=65a84e8c&is=6595d98c&hm=193066d6784161ccd6c29eb02152282b598275c3a87ed966e99051ab31450d6b&"
+    />
+  ),
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  // ... other theme options
+};
